a007211a57057f235a53e234444403d02f1b93b2
NayOoLwin5 <suman.nay17half1999@gmail.com>
2024-10-22 15:49:45 +0700

    cimb5

--- cimb.py
+++ cimb.py
@@ -72,81 +72,64 @@ def batch_pulling_task(device_id):
 
     print("'Recent' text field appeared. Starting batch pulling...")
 
+    segment_line_class = "android.widget.LinearLayout"
+    recycler_view_id = "id.co.cimbniaga.mobile.android:id/rv_transaction"
     transaction_title_id = "id.co.cimbniaga.mobile.android:id/tv_remarks_title"
     transaction_date_id = "id.co.cimbniaga.mobile.android:id/tv_remarks_date"
+    transaction_value_id = "id.co.cimbniaga.mobile.android:id/tv_transaction_value"
     memo1_id = "id.co.cimbniaga.mobile.android:id/tv_memo1"
     memo2_id = "id.co.cimbniaga.mobile.android:id/tv_memo2"
-    transaction_value_id = "id.co.cimbniaga.mobile.android:id/tv_transaction_value"
-    segment_line_class = "android.widget.LinearLayout"
+    memo3_id = "id.co.cimbniaga.mobile.android:id/tv_memo3"
+
 
     printed_items = set()
 
     while True:
         try:
-            transaction_titles = d(resourceId=transaction_title_id)
-            transaction_dates = d(resourceId=transaction_date_id)
-            memos1 = d(resourceId=memo1_id)
-            memos2 = d(resourceId=memo2_id)
-            transaction_values = d(resourceId=transaction_value_id)
+            recycler_view = d(resourceId=recycler_view_id)
+            transaction_items = recycler_view.child(className="android.view.ViewGroup")
 
             transactions = []
-            for title, date, memo1, memo2, value in zip(transaction_titles, transaction_dates, memos1, memos2, transaction_values):
-                if title.get_text() == "ATM BERSAMA CHARGES":
-                    transaction = (
-                        title.get_text(),
-                        date.get_text(),
-                        value.get_text()
-                    )
-                else:
-                    transaction = (
-                        title.get_text(),
-                        date.get_text(),
-                        memo1.get_text(),
-                        memo2.get_text(),
-                        value.get_text()
-                    )
-                if transaction not in printed_items:
+            for i, item in enumerate(transaction_items):
+                if i >= 3:
+                    break
+                visible_texts = item.child(className="android.widget.TextView")
+                
+                transaction = tuple(text.get_text() for text in visible_texts if text.get_text() != '00' and 'IDR' not in text.get_text())
+                
+                transaction_value = item.child(resourceId=transaction_value_id).get_text()
+                if any('IDR' in text.get_text() for text in visible_texts):
+                    transaction += (transaction_value,)
+                if transaction and transaction not in printed_items:
                     transactions.append(transaction)
                     printed_items.add(transaction)
             
             for transaction in transactions:
-                if len(transaction) == 3:
+                try:
                     formatted_date = time.strftime('%Y-%m-%d', time.strptime(transaction[1], '%d %b %Y %H:%M'))
                     formatted_datetime = time.strftime('%Y-%m-%dT%H:%M:%S+0700', time.strptime(transaction[1], '%d %b %Y %H:%M'))
                     title_text = transaction[0]
-                    amount_text = transaction[2]
+                    memo1_text = "?" if len(transaction) <= 3 or transaction[2] == "-" else transaction[2]
+                    memo2_text = "?" if len(transaction) <= 4 or transaction[3] == "-" else transaction[3]
+                    memo3_text = "?" if len(transaction) <= 5 or transaction[4] == "-" else transaction[4]
+                    amount_text = transaction[-1]
                     transaction_type = "DB" if "-" in amount_text else "CR" if "+" in amount_text else "?"
                     amount_numeric = float(amount_text.replace("IDR", "").replace("-", "").replace("+", "").replace(",", "").strip())
-                    result = f"{formatted_date} | {title_text} -- {amount_text} -- {formatted_datetime}| {transaction_type} | {amount_numeric} | {str(float(0))}"
-                else:
-                    formatted_date = time.strftime('%Y-%m-%d', time.strptime(transaction[1], '%d %b %Y %H:%M'))
-                    formatted_datetime = time.strftime('%Y-%m-%dT%H:%M:%S+0700', time.strptime(transaction[1], '%d %b %Y %H:%M'))
-                    tnx_id_text = transaction[2]
-                    title_text = transaction[0]
-                    remark_text = transaction[3]
-                    amount_text = transaction[4]
-                    transaction_type = "DB" if "-" in amount_text else "CR" if "+" in amount_text else "?"
-                    amount_numeric = float(amount_text.replace("IDR", "").replace("-", "").replace("+", "").replace(",", "").strip())
-                    result = f"{formatted_date} | {title_text} -- {tnx_id_text} -- {remark_text} -- {amount_text} -- {formatted_datetime}| {transaction_type} | {amount_numeric} | {str(float(0))}"
-                    # result = " | ".join([
-                    # formatted_date,
-                    # " -- ".join([
-                    #     title_text,
-                    #     remark_text,
-                    #     amount_text,
-                    #     formatted_datetime,
-                    #     transaction_type
-                    # ]),
-                    # str(amount_numeric * 1000),  # Convert float to string
-                    # str(float(0))  # Convert to string
-                    # ]
-                    # )
-                print(result)
+                    memos = [memo1_text, memo2_text, memo3_text]
+                    non_question_memos = [memo for memo in memos if memo != "?"]
+                    question_memos = [memo for memo in memos if memo == "?"]
+                    all_memos = " -- ".join(non_question_memos + question_memos)
+                    result = f"{formatted_date} | {title_text} -- {all_memos} -- {amount_text} -- {formatted_datetime} | {transaction_type} | {amount_numeric} | {str(float(0))}"
+                    print(result)
+                except IndexError as e:
+                    print(f"Error processing transaction: {transaction}. Error: {str(e)}")
+                except ValueError as e:
+                    print(f"Error parsing transaction data: {transaction}. Error: {str(e)}")
 
-            previous_transactions = [t.info for t in d(resourceId=transaction_title_id)]
+            previous_transactions = [t.info for t in recycler_view.child(resourceId=transaction_title_id)]
 
             # Get the bounds of the last transaction item
-            last_transaction = d(resourceId=transaction_title_id).info['bounds']
+            last_transaction = recycler_view.child(resourceId=transaction_title_id)[-1].info['bounds']
             start_x = (last_transaction['left'] + last_transaction['right']) // 2
 
             # Get the bounds of the 'Recent' field for the end position
@@ -175,12 +158,20 @@ def batch_pulling_task(device_id):
                 print("No more transactions found. Exiting batch pulling.")
                 break
 
-        except UiObjectNotFoundError:
-            print("No more transactions found or failed to scroll. Exiting batch pulling.")
+        except UiObjectNotFoundError as e:
+            print(f"No more transactions found or failed to scroll. Error: {str(e)}. Exiting batch pulling.")
             break
 
-device_id = "100.79.114.17:5555"
+def write_hierarchy(device_id):
+    xml_content = device_id.dump_hierarchy()
+    with open("output.xml", "w") as f:
+        f.write(xml_content)    
+    print("UI hierarchy saved to hierarchy.xml")
+
+device_id = "100.79.114.17:5556"
 try:
     batch_pulling_task(device_id)
+    # d = u2.connect(device_id)
+    # write_hierarchy(d)
 except Exception as e:
     print(f"Unexpected error occurred: {e}")
